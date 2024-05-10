7a68e56157c757612fd76522bd8e2576bd91455b
MOMIN5 <drmine05@gmail.com>
2024-05-10 16:42:49 +0530

    reader and logger complete

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::{fs::File, io::{Write, Read}, collections::HashMap};
+use std::{fs::File, io::{Write, Read}, collections::HashMap, thread, time::Duration};
 
 use winapi;
 use sysinfo::{System,Pid};
 
 fn main() {
-    let pid = get_foreground_process();
-    let name = get_process_name(pid);
-    
-    update_time(name);
-    //println!("{}",name);
+    loop{
+        let pid = get_foreground_process();
+        let name = get_process_name(pid);
+
+        update_time(name);
+        thread::sleep(Duration::from_secs(3));
+    }
 }
 
 fn get_foreground_process() -> u32 {
@@ -38,13 +40,13 @@ fn get_process_name(current_pid: u32) -> String{
 }
 
 fn update_time(app_name : String) {
-    let time_stamp = 5;
+    let time_stamp = 3;
     let file_path = "app.log";
     
     let mut app_map = read_file();
     
-    let modif_data = String::from(app_name.as_str()) + ":" + time_stamp.to_string().as_str();
-    let data_byte = modif_data.as_bytes();
+    //let modif_data = String::from(app_name.as_str()) + ":" + time_stamp.to_string().as_str();
+    //let data_byte = modif_data.as_bytes();
     
     let mut file = File::options().write(true).open(file_path).unwrap();
     
@@ -59,20 +61,6 @@ fn update_time(app_name : String) {
     for (name, time) in app_map.iter() {
         writeln!(file, "{}:{}",name,time).unwrap();
     }
-    
-    //println!("{:?}",)
-    /*match file {
-        Ok(mut f) => {
-            f.write_all(data_byte);
-            println!("h")
-        }
-        Err(_) => {
-            let mut f = File::create(file_path).unwrap();
-            
-            f.write_all(data_byte);
-            println!("b")
-        }
-    }*/
 }
 
 fn read_file() -> HashMap<String,i32> {
@@ -80,22 +68,24 @@ fn read_file() -> HashMap<String,i32> {
     
     let file = File::open(file_path);
     let mut map: HashMap<String, i32> = HashMap::new();
-    
+
     match file {
         Ok(mut f) => {
             let mut data_str = String::new();
             f.read_to_string(&mut data_str);
-            
+
             let lines = data_str.split("\n");
-            
+
             for line in lines {
                 if let Some((name, time)) = line.split_once(":") {
+                    //println!("{}",time);
                     map.insert(name.to_string(), time.parse::<i32>().unwrap());
                 } 
             }
         }
         Err(_) => {
-            let f = File::create(file_path).unwrap();
+            let
+            f = File::create(file_path).unwrap();
         }
     }
     return map;
